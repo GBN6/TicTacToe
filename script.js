558e1328be4c08f56a26838e4accd3e313eb81b6
GBN6 <czesiox8@gmail.com>
2022-06-26 13:54:30 +0200

    Add initial clickable gameboard display

--- script.js
+++ script.js
@@ -1,7 +1,37 @@
-const gameBoard = (() => {
 
-});
+const gameBoard = (() => {
+    const gameTile = document.querySelectorAll('.game-tile');
+    const playerTurn = document.querySelector('.player-turn');
+    let flagMarker = 0;
+    playerTurn.textContent = 'X\'s turn'; 
+    const placeMarker = (e) => {
+        if (e.target.textContent === '')
+        {
+            if (flagMarker === 0)
+            {
+                e.target.textContent = 'X';
+                playerTurn.textContent = 'O\'s turn';
+                flagMarker = 1;
+            }
+            else if (flagMarker === 1)
+            {
+                e.target.textContent = 'O';
+                playerTurn.textContent = 'X\'s turn';
+                flagMarker = 0;
+            }
+        }
+    }
+
+    gameTile.forEach((item) => item.addEventListener('click', placeMarker))
+})();
 
 const player = (name, marker) => {
     return {name, marker};
 }
+
+// gameBoard.renderBoard;
+
+
+// const jeff = player('jeff', 'x')
+// console.log(jeff.marker);
+
